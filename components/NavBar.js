df8c07b027c09d50606e0b87f4f27eda6c2adc66
Simon Oh <dev.ohss@gmail.com>
2023-06-28 22:30:34 -0400

    #2.1 Fetching Data

--- components/NavBar.js
+++ components/NavBar.js
@@ -5,24 +5,44 @@ export default function NavBar() {
   const router = useRouter();
   return (
     <nav>
-      <Link href="/" className={router.pathname === "/" ? "active" : ""}>
-        Home
-      </Link>
-      <Link
-        href="/about"
-        className={router.pathname === "/about" ? "active" : ""}
-      >
-        About
-      </Link>
+      <img src="/vercel.svg" />
+      <div>
+        <Link href="/" className={router.pathname === "/" ? "active" : ""}>
+          Home
+        </Link>
+        <Link
+          href="/about"
+          className={router.pathname === "/about" ? "active" : ""}
+        >
+          About
+        </Link>
+      </div>
+
       <style jsx>{`
         nav {
-          background-color: tomato;
+          display: flex;
+          gap: 10px;
+          flex-direction: column;
+          align-items: center;
+          padding-top: 20px;
+          padding-bottom: 10px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
         }
-        a {
-          text-decoration: none;
+        img {
+          max-width: 100px;
+          margin-bottom: 5px;
+        }
+        nav a {
+          font-weight: 600;
+          font-size: 18px;
         }
         .active {
           color: yellow;
+        }
+        nav div {
+          display: flex;
+          gap: 10px;
         }
       `}</style>
     </nav>
